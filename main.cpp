052050740f86075a4ff6f6319ebbae5df8e96765
A Student <student@nowhere.edu>
2026-04-14 01:51:12 +0000

    Apr 13, 2026, 6:51 PM

--- main.cpp
+++ main.cpp
@@ -22,6 +22,6 @@ int main()
 
   double temp = vy * vy +2 * g * h;
   double time = (vy * sqrt(temp))/g;
-  double maxH = h + (vy* vy) / 
+  double maxH = h + (vy* vy) / (2 * g);
   cout << "Hello world" << endl;
 }
