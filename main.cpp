f515e07dc274d7171b6a45531bf526ab1a7b1819
A Student <student@nowhere.edu>
2026-04-14 01:12:03 +0000

    Apr 13, 2026, 6:12 PM

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main()
-{ const double g = 32.17405;
-  const double pi = 3.141592654
+{ 
+  const double g = 32.17405;
+  const double pi = 3.141592654;
+
   cout << "Hello world" << endl;
 }
